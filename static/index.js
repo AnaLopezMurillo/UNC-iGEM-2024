9edb4a12ab019b66b0ef0db897688e0b2ce483cb
Isabel Lopez Murillo <isaana@email.unc.edu>
2024-05-26 22:51:19 -0400

    Switched JS to defer
    Got rid of some comments and for readability purposes got rid of JS loading event listener in favor of changing the file to defer on load in layout.html

--- static/index.js
+++ static/index.js
@@ -1,48 +1,47 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggles = document.querySelectorAll('.dropdown-toggle-toc');
-    const items = document.querySelectorAll('.toc-list-items');
+// ToC functions
+const toggles = document.querySelectorAll('.dropdown-toggle-toc');
+const items = document.querySelectorAll('.toc-list-items');
 
-    toggles.forEach(function(toggle) {
-        toggle.addEventListener('click', function(event) {
-            event.stopPropagation();
-            const dropdown = toggle.nextElementSibling;
-            const isOpen = dropdown.classList.contains('show');
+toggles.forEach(function(toggle) {
+    toggle.addEventListener('click', function(event) {
+        event.stopPropagation();
+        const dropdown = toggle.nextElementSibling;
+        const isOpen = dropdown.classList.contains('show');
 
-            items.forEach(function(item) {
-                item.classList.remove('show');
-                item.style.maxHeight = null;
-            });
-
-            if (!isOpen) {
-                dropdown.classList.add('show');
-                dropdown.style.maxHeight = dropdown.scrollHeight + "px";
-            } else {
-                dropdown.classList.remove('show');
-                dropdown.style.maxHeight = null;
-            }
-        });
-    });
-
-    window.addEventListener('click', function() {
         items.forEach(function(item) {
             item.classList.remove('show');
             item.style.maxHeight = null;
         });
-    });
 
-    const tocLinks = document.querySelectorAll('.toc-list-item a');
-    tocLinks.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const targetId = link.getAttribute('href').substring(1);
-            const targetEl = document.getElementById(targetId);
-            const offset = window.innerHeight / 3;
-            const targetPos = targetEl.getBoundingClientRect().top + window.scrollY - offset;
-            window.scrollTo({
-                top: targetPos,
-                behavior: "smooth"
-            });
-        })
-
+        if (!isOpen) {
+            dropdown.classList.add('show');
+            dropdown.style.maxHeight = dropdown.scrollHeight + "px";
+        } else {
+            dropdown.classList.remove('show');
+            dropdown.style.maxHeight = null;
+        }
     });
+});
+
+window.addEventListener('click', function() {
+    items.forEach(function(item) {
+        item.classList.remove('show');
+        item.style.maxHeight = null;
+    });
+});
+
+const tocLinks = document.querySelectorAll('.toc-list-item a');
+tocLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetEl = document.getElementById(targetId);
+        const offset = window.innerHeight / 3;
+        const targetPos = targetEl.getBoundingClientRect().top + window.scrollY - offset;
+        window.scrollTo({
+            top: targetPos,
+            behavior: "smooth"
+        });
+    })
+
 });
