f55f62d0a91736df763fe66a2c3585a3b1b804f6
Isabel Lopez Murillo <isaana@email.unc.edu>
2024-06-28 14:41:44 -0400

    Add data to index.js
    will try to auto-populate the teams section w/ styling we have rn

--- static/index.js
+++ static/index.js
@@ -45,3 +45,66 @@ tocLinks.forEach(function(link) {
     })
 
 });
+
+// populate team.html w/ members data
+const teamPrimary = {
+    "Annika Bridge" : "Human Practices Co-lead",
+    "Anusha Gupta" : "Secretary",
+    "Corinne Drabenstott" : "",
+    "Daniellejen Nares" : "Fundraising",
+    "Dominic Kubissa" : "Human Practices",
+    "Elizabeth Krist" : "Dry Lab",
+    "Gavin Wiltshire" : "Team Lead",
+    "Grant Holland" : "Wet Lab",
+    "Heng Annika Shi" : "Social Media Lead",
+    "Isabel Lopez Murillo" : "Wiki Lead",
+    "Ishita Siddamreddy" : "Wiki",
+    "Jack Titus" : "Social Media",
+    "Joshua Lopez" : "Wet Lab Lead",
+    "Julia Zhu" : "",
+    "Keshav Saxena" : "",
+    "Lahari  Pokala" : "Wet Lab",
+    "Layla Gusa" : "Wiki",
+    "Logan Amos": "Human Practices",
+    "Moiz Chomelawala": "",
+    "Nishant Dengi" : "Wet Lab",
+    "Osvaldo Linares": "Dry Lab",
+    "Pristine Onuoha" : "Human Practices Co-Lead",
+    "Samantha Carew" : "",
+    "Sriya Darsi" : "Human Practices",
+    "Uditi Patil" : "Wet Lab",
+    "Zaid Syed" : "Wet Lab",
+}
+
+const teamSecondary = {
+    "Annika Bridge" : "",
+    "Anusha Gupta" : "Literature Review, Project Ideation",
+    "Corinne Drabenstott" : "",
+    "Daniellejen Nares" : "",
+    "Dominic Kubissa" : "",
+    "Elizabeth Krist" : "Human Practices, Fundraising",
+    "Gavin Wiltshire" : "",
+    "Grant Holland" : "Literature Review, Project Ideation",
+    "Heng Annika Shi" : "Human Practices, Collaboration Lead, Fundraising",
+    "Isabel Lopez Murillo" : "",
+    "Ishita Siddamreddy" : "",
+    "Jack Titus" : "",
+    "Joshua Lopez" : "Literature Review, Project Ideation, Dry Lab",
+    "Julia Zhu" : "",
+    "Keshav Saxena" : "",
+    "Lahari  Pokala" : "Literature Review, Project Ideation, Dry Lab",
+    "Layla Gusa" : "",
+    "Logan Amos": "Human Practices",
+    "Moiz Chomelawala": "",
+    "Nishant Dengi" : "Literature Review, Project Ideation",
+    "Osvaldo Linares": "Wet Lab, Fundraising",
+    "Pristine Onuoha" : "",
+    "Samantha Carew" : "",
+    "Sriya Darsi" : "",
+    "Uditi Patil" : "Literature Review, Project Ideation",
+    "Zaid Syed" : "Literature Review, Project Ideation",
+}
+
+for (let member of Object.keys(teamPrimary)) {
+    
+}
