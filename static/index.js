dd4ec6b15acef83bf45147d529553e5a39df9f07
Isabel Lopez Murillo <97560944+AnaLopezMurillo@users.noreply.github.com>
2024-07-03 08:47:07 -0400

    Merge Team Page into Main (#4)
    * Add data to index.js
    
    will try to auto-populate the teams section w/ styling we have rn
    
    * Auto-populate team.html w/ members
    
    Switched to automatic population using index.js, easier to handle + add ids when we end up doing that
    
    * Remove console.log message
    
    whoops
    
    * Add pop-up styling
    
    * Add popup hide event listener
    
    * new edits
    
    * Re-add team auto-pop. + pop-up
    
    JS/CSS stylesheet + team.html were changed
    
    ---------
    
    Co-authored-by: Ishita Siddamreddy <ishitasr@unc.edu>

--- static/index.js
+++ static/index.js
@@ -45,3 +45,110 @@ tocLinks.forEach(function(link) {
     })
 
 });
+
+// populate team.html w/ members data
+const teamPrimary = {
+    "Gavin Wiltshire" : "Team Lead",
+    "Corinne Drabenstott" : "",
+    "Annika Bridge" : "Human Practices Co-lead",
+    "Pristine Onuoha" : "Human Practices Co-Lead",
+    "Dominic Kubissa" : "Human Practices",
+    "Logan Amos": "Human Practices",
+    "Sriya Darsi" : "Human Practices",
+    "Anusha Gupta" : "Secretary",
+    "Daniellejen Nares" : "Fundraising",
+    "Isabel Lopez Murillo" : "Wiki Lead",
+    "Ishita Siddamreddy" : "Wiki",
+    "Layla Gusa" : "Wiki",
+    "Joshua Lopez" : "Wet Lab Lead",
+    "Lahari  Pokala" : "Wet Lab",
+    "Grant Holland" : "Wet Lab",
+    "Nishant Dengi" : "Wet Lab",
+    "Uditi Patil" : "Wet Lab",
+    "Zaid Syed" : "Wet Lab",
+    "Heng Annika Shi" : "Social Media Lead",
+    "Jack Titus" : "Social Media",
+    "Elizabeth Krist" : "Dry Lab",
+    "Osvaldo Linares": "Dry Lab",
+    "Julia Zhu" : "",
+    "Keshav Saxena" : "",
+    "Moiz Chomelawala": "",
+    "Samantha Carew" : "",
+}
+
+const teamSecondary = {
+    "Gavin Wiltshire" : "",
+    "Corinne Drabenstott" : "",
+    "Annika Bridge" : "",
+    "Pristine Onuoha" : "",
+    "Anusha Gupta" : "Literature Review, Project Ideation",
+    "Daniellejen Nares" : "",
+    "Dominic Kubissa" : "",
+    "Elizabeth Krist" : "Human Practices, Fundraising",
+    "Grant Holland" : "Literature Review, Project Ideation",
+    "Heng Annika Shi" : "Human Practices, Collaboration Lead, Fundraising",
+    "Isabel Lopez Murillo" : "",
+    "Ishita Siddamreddy" : "",
+    "Jack Titus" : "",
+    "Joshua Lopez" : "Literature Review, Project Ideation, Dry Lab",
+    "Julia Zhu" : "",
+    "Keshav Saxena" : "",
+    "Lahari  Pokala" : "Literature Review, Project Ideation, Dry Lab",
+    "Layla Gusa" : "",
+    "Logan Amos": "Human Practices",
+    "Moiz Chomelawala": "",
+    "Nishant Dengi" : "Literature Review, Project Ideation",
+    "Osvaldo Linares": "Wet Lab, Fundraising",
+    "Samantha Carew" : "",
+    "Sriya Darsi" : "",
+    "Uditi Patil" : "Literature Review, Project Ideation",
+    "Zaid Syed" : "Literature Review, Project Ideation",
+}
+
+let i = 0;
+for (let member of Object.keys(teamPrimary)) {
+    let div = document.createElement("div");
+    let team_img = document.createElement("div");
+    let name = document.createElement("div");
+    let job = document.createElement("div");
+
+    div.classList.add("person");
+    div.id = member;
+    team_img.classList.add("team_image");
+
+    // append member name
+    name.classList.add("name");
+    name.innerHTML = member;
+
+    // append member primary job
+    job.classList.add("job");
+    job.innerHTML = teamPrimary[member];
+
+    // append divs to each other
+    div.appendChild(team_img);
+    div.appendChild(name);
+    div.appendChild(job);
+
+    // grab members div from team.html + append
+    let members = document.getElementsByClassName("members")[0];
+    members.appendChild(div);
+}
+
+const members = document.querySelectorAll("div.person");
+
+// going to need to change the way we get this
+const popup = document.getElementById("test");
+members.forEach(function(member) {
+    member.addEventListener('click', function(event) {
+        event.stopPropagation();
+        popup.style.display = "block";
+        popup.style.visibility = "visible";
+    })
+});
+
+window.addEventListener('click', function(event) {
+    if (!document.getElementById('test').contains(event.target)) {
+        popup.style.display = 'none';
+        popup.style.visibility = 'hidden';
+    }
+});
