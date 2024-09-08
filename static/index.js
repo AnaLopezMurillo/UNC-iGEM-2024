0eabff78c0f3c8a0391da98f47aa7d89a8d5ec9e
Isabel Lopez Murillo <isaana@email.unc.edu>
2024-09-08 18:11:27 -0400

    Fix notebook JS, add styling
    slowly getting to what the figma has

--- static/index.js
+++ static/index.js
@@ -45,3 +45,41 @@ tocLinks.forEach(function(link) {
     })
 
 });
+
+function setIndex(){
+    let pages = document.querySelectorAll(".page")
+    for(var i=0;i<pages.length;i++){
+      pages[i].style.zIndex = pages.length - i;
+    }
+  }
+
+setIndex();
+
+function previous(){
+    let active = document.querySelector(".active");
+    active.style.transform = "rotateY(0deg)";
+    if (active.previousElementSibling.classList.contains("page")) {
+        let prevSib = active.previousElementSibling;
+        console.log(prevSib)
+        active.className="page";
+        prevSib.className = "page active";
+        setIndex();
+        prevSib.style.transform = "rotateY(180deg)";
+        prevSib.style.zIndex = "9998";
+    }
+}
+
+function next(){
+    let active = document.querySelector(".active")
+    let nextSib = active.nextElementSibling;
+    if (nextSib == null) {
+        nextSib = active;
+    }
+    active.style.transform = "rotateY(180deg)";
+    active.className="page";
+    setIndex();
+    active.style.zIndex = "9998";
+    if (nextSib){
+        nextSib.className = "page active";
+    }
+}
