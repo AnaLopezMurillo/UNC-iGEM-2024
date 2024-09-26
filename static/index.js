c8b96471130a4c2b0bcd14a96d993df2ba3cbcde
Isabel Lopez Murillo <isaana@email.unc.edu>
2024-09-26 19:40:29 -0400

    Reconfiguring notebook js
    got lost in last merge

--- static/index.js
+++ static/index.js
@@ -266,3 +266,55 @@ function handleClickOutside(event) {
     // remove the event listener when user clicks out
     document.removeEventListener('click', handleClickOutside);
   }
+
+  function setIndex(){
+    let pages = document.querySelectorAll(".page")
+    for(var i=0;i<pages.length;i++){
+      pages[i].style.zIndex = pages.length - i;
+    }
+  }
+
+setIndex();
+
+function previous(){
+    let active = document.querySelector(".active");
+    active.style.transform = "rotateY(0deg)";
+    if (active.previousElementSibling.classList.contains("page")) {
+        let prevSib = active.previousElementSibling;
+        active.className="page";
+        prevSib.className = "page active";
+        setIndex();
+        prevSib.style.transform = "rotateY(180deg)";
+        prevSib.style.zIndex = "9997";
+    }
+}
+
+function next(){
+    let active = document.querySelector(".active")
+    let nextSib = active.nextElementSibling;
+    if (nextSib == null) {
+        nextSib = active;
+    }
+    active.style.transform = "rotateY(180deg)";
+    active.className="page";
+    setIndex();
+    active.style.zIndex = "9997";
+    if (nextSib){
+        nextSib.className = "page active";
+    }
+}
+
+// adds bookmarks to notebook page at each header (new day) instead of having to do it manually !!
+function add_bookmarks() {
+    let page_headers = document.querySelectorAll('div.page-header');
+    page_headers.forEach(function(header) {
+        header.insertAdjacentHTML('beforebegin', 
+            '<div class="bookmark"> <img src="../../static/bookmark.png"></div>'
+        )
+    })
+}
+
+add_bookmarks();
+
+// notebook page flip
+const nbWrapper = document.querySelector('.nb-wrapper');
